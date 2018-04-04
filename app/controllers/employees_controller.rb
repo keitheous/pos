c26c07f8ecffbdaea03062cc8a2aab8e7ebba940
keitheous <keithchongwy@gmail.com>
2018-04-04 14:42:16 +1000

    employees controller

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,20 @@
+class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
